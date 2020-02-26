eecd52e11c58c6c5e8e2df4ab0a440c341ca7eea
Forrest <Forrest@Forrests-MacBook-Pro.local>
2020-02-25 20:33:03 -0600

    small function changes to push me in the right direction

--- Unsolved/script.js
+++ Unsolved/script.js
@@ -1,10 +1,3 @@
-// FOR TIMER
-// set minutes with minutes working
-// if seconds is at 0 subtract one from minutes
-// reset seconds to 60
-// each time seconds hits 00 subtract 1 from minutes total
-// if minutes and seconds both = 0 time is up. 
-
 var statusSpan = document.querySelector("#status");
 var statusToggle = document.querySelector("#status-toggle");
 var playButton = document.querySelector("#play");
@@ -17,7 +10,14 @@ var restMinutesInput = document.querySelector("#rest-minutes");
 
 var totalSeconds = 0;
 var secondsElapsed = 0;
-var interval = setInterval(startTimer, 1000);
+// var isPaused = false;
+var isWorking = false;
+var interval;
+
+// launches app by calling settime() and renderTime()
+// gettimepreferences()
+
+setInterval(startTimer, 1000);
 
 initTimer();
 // startTimer();
@@ -25,27 +25,42 @@ initTimer();
 // turns workMinutesinput into seconds 
 function initTimer(){
   minutesDisplay.textContent = workMinutesInput.value;
-  var seconds = workMinutesInput.value * 60;
-  return totalSeconds = seconds;
+  var secondsStart = workMinutesInput.value * 60;
+  totalSeconds = secondsStart;
 }
+
+
 console.log(totalSeconds);
 
+function getFormattedMinutes(){
+  var secondsLeft = totalSeconds - secondsElapsed;
 
+}
+
+function getFormattedSeconds(){
+
+}
+
+function secondsElapsed(){
+  for (var i = 0; i < totalSeconds; i++){
+    secondsElapsed++;
+    console.log(secondsElapsed);
+  }
+}
 
 // What runs when the timer is started.
-function startTimer() {
-  // Write code to start the timer here
-  totalSeconds--;
-  console.log(totalSeconds); 
-}  
-
 // KILL TIMER
 function stopTimer(){
   clearInterval(interval);
 }
 
 // playButton.addEventListener("click", initTimer);
-playButton.addEventListener("click", startTimer);
+playButton.addEventListener("click", function () {
+  // Write code to start the timer here
+  totalSeconds--;
+  console.log(totalSeconds); 
+});
+
 pauseButton.addEventListener("click", stopTimer)
 workMinutesInput.addEventListener("keypress", function(e){
   if(e === "Enter"){
