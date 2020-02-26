09b79480410515cdb2c922df69d1b19dd956c52c
Forrest Miller <Forrestmillerj@gmail.com>
2020-02-25 21:42:21 -0600

    made Timer

--- Unsolved/script.js
+++ Unsolved/script.js
@@ -10,60 +10,142 @@ var restMinutesInput = document.querySelector("#rest-minutes");
 
 var totalSeconds = 0;
 var secondsElapsed = 0;
-// var isPaused = false;
-var isWorking = false;
+var status = "working";
 var interval;
 
-// launches app by calling settime() and renderTime()
-// gettimepreferences()
+// this launches the app by calling setTime() and renderTime()
+getTimePreferences();
 
-setInterval(startTimer, 1000);
-
-initTimer();
-// startTimer();
-
-// turns workMinutesinput into seconds 
-function initTimer(){
-  minutesDisplay.textContent = workMinutesInput.value;
-  var secondsStart = workMinutesInput.value * 60;
-  totalSeconds = secondsStart;
-}
-
-
-console.log(totalSeconds);
-
+// these 2 functions are jsut for making sure the numbers look nice for the html elements
 function getFormattedMinutes(){
+
   var secondsLeft = totalSeconds - secondsElapsed;
 
-}
+  var minutesLeft = Math.floor(secondsLeft / 60);
 
-function getFormattedSeconds(){
+  var formattedMinutes;
 
-}
-
-function secondsElapsed(){
-  for (var i = 0; i < totalSeconds; i++){
-    secondsElapsed++;
-    console.log(secondsElapsed);
+  if (parseInt(minutesLeft) < 10) {
+    formattedMinutes = "0" + minutesLeft;
+  } else {
+    formattedMinutes = minutesLeft;
   }
+
+  return formattedMinutes;
 }
 
-// What runs when the timer is started.
-// KILL TIMER
-function stopTimer(){
+function getFormattedSeconds() {
+  var secondsLeft = (totalSeconds - secondsElapsed) % 60;
+
+  var formattedSeconds;
+
+  if (secondsLeft < 10) {
+    formattedSeconds = "0" + secondsLeft;
+  } else {
+    formattedSeconds = secondsLeft;
+  }
+  return formattedSeconds;
+}
+
+function getTimePreferences(){
+  // here we check to see if any preferences have been set in the local storage via " setTimePreferences"
+  var preferences = JSON.parse(localStorage.getItem("preferences"));
+
+  // setTimePreferences() is actually never called so the below code will never run.
+  // (ie. the "preferences" variable checked below will always be null. ) The settings are always set by directly checking the input elementsin the setTime() function
+  if (preferences) {
+    if(preferences.workMinutes) {
+      workMinutesInput.value = preferences.workMinutes;
+    }
+
+    if (preferences.restMinutes) {
+      restMinutesInput.value = preferences.restMinutes;
+    }
+  }
+  // this is where the app si really kicked-off, setTime and renderTime are the two main routines. 
+  setTime();
+  renderTime();
+}
+
+// this function just retrieves the value from the html input elelemnts 
+// it sets the totalSeconds variable which is used in etFormattedMinutes/Seconds() and the renderTime() function.
+// it essentially resets our timer.
+function setTime(){
+  var minutes;
+  
+  if (status === "working"){
+    minutes = workMinutesInput.value.trim();
+  } else {
+    minutes = restMinutesInput.value.trim(); 
+  }
+
   clearInterval(interval);
+  totalSeconds = minutes * 60;
 }
 
-// playButton.addEventListener("click", initTimer);
-playButton.addEventListener("click", function () {
-  // Write code to start the timer here
-  totalSeconds--;
-  console.log(totalSeconds); 
-});
+// This function does 2 things. displays the time and checks to see if time is up.
+function renderTime(){
+  // when renderTime is called it sets tne textconetent for the timer html
+  minutesDisplay.textContent = getFormattedMinutes();
+  secondsDisplay.textContent = getFormattedSeconds();
 
-pauseButton.addEventListener("click", stopTimer)
-workMinutesInput.addEventListener("keypress", function(e){
-  if(e === "Enter"){
-    initTimer();  
+  //... and then checks to see if the tieme has run out
+  if (secondsElapsed >= totalSeconds){
+    if( status === "working"){
+      alert("time for a break!");
+    } else {
+      alert("time to get back to work!!");
+    }
+
+    stopTimer();
   }
-  });
+}
+//  This function stops the setInterval() set in startTimer but does not reset the seconds variable and does not reset the time by calling setTime().
+function pauseTimer(){
+  clearInterval(interval);
+  renderTime();
+}
+
+// this function stops the intercal and also resets secondsElapsed and calls setTime() which effectively resetst the timer to theinput selections workMinutesInput.value and restMinutesInput.value
+function stopTimer(){
+  secondsElapsed = 0;
+  setTime();
+  renderTime();
+}
+
+function toggleStatus(event) {
+  var checked = event.target.checked;
+
+  if (checked) {
+    status = "Working";
+  } else {
+    status = "Resting";
+  }
+
+  statusSpan.textContent = status;
+}
+
+// This function is where the "time" aspect ogthe timer runs 
+// Notice no settings are checnged other than to increment the secondsElapsed var
+function startTimer() {
+  setTime;
+
+  // we only want to start the timer if minutes is > 0
+  if (totalSeconds > 0) {
+    // the "interval" varible here using "setinterval()" begins the recurring increment of the secondsElapsed variable which is used to check if the time is up
+    interval = setInterval(function() {
+      secondsElapsed++;
+      // so renderTime() is called here once every second
+      renderTime();
+    }, 1000);
+  } else {
+    alert("minutes of work/rest must be greater than 0.")
+  }
+}
+
+
+
+playButton.addEventListener("click", startTimer);
+pauseButton.addEventListener("click", pauseTimer);
+stopButton.addEventListener("click", stopTimer);
+statusToggle.addEventListener("change", toggleStatus);
